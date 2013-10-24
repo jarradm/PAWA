78bd679e5dc50387e880cfe05fc61010f9243b15
Jarrad Merrick <merrickj@live.com>
2013-10-25 00:30:08 +0800

    Added DisplayImage(page) and a couple of bug fixes in shared view files

--- PAWA/Controllers/ImageController.cs
+++ PAWA/Controllers/ImageController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PAWA.DAL;
+using PAWA.Models;
 
 namespace PAWA.Controllers
 {
@@ -21,7 +23,28 @@ namespace PAWA.Controllers
 
         public ActionResult DisplayImage()
         {
-            return View();
+            int UserID = 1;
+            PAWAContext dbContext = new PAWAContext();
+
+            IEnumerable<File> files = from f in dbContext.Files
+                        where f.UserID == UserID
+                        select f;
+
+            var file = new File
+            {
+                UserID = 1,
+                TypeID = 1,
+                FolderID = 2,
+                Tags = "1,2",
+                Filename = "Koala.jpg",
+                UploadedDateTime = DateTime.Parse("2013/08/27 17:16:15"),
+                Description = "Sydney Harbour as seen from the peak of the Sydney Harbour Bridge",
+                SizeMB = 3500,
+                SizeHeight = 680,
+                SizeWidth = 1048
+            };
+
+            return View(file);
         }
 
         //
