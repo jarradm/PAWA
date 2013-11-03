3d3cadd2f6ec421c1691d9406c3351899752ded1
Oliver Buckler <obsta2010@hotmail.com>
2013-11-03 17:31:49 +0800

    Tags.Usecount now increments on every use, methods in Tools.cs have tooltips.

--- PAWA/Classes/Tools.cs
+++ PAWA/Classes/Tools.cs
@@ -17,14 +17,13 @@ namespace PAWA.Classes
        public static int totalImageCount {get; set;} //counter for index adding
        private static List<int> selectedfile { get; set; } //list containing ids
        public static int UserID { get; set; }
-        /*
-           *  ImageResize(Image, Size)
-           *  Take in image, create a new bitmap, bitmap size = newsize
-           *  Render and clean image
-           *  Apply image to bitmap
-           *  
-           *  Return the thumbnail
-        */
+        /// <summary>
+           ///  Take in image, create a new bitmap, bitmap size = newsize
+           ///  Render and clean image
+           ///  Apply image to bitmap
+           ///  
+           ///  Return the thumbnail
+       /// </summary>
         public Image ImageResize(Image imgFile, Size newSize)
         {
             
@@ -39,12 +38,10 @@ namespace PAWA.Classes
             }
             return newImage;
         }
-        /*
-         * checkExtension(string)
-         * 
-         * Checks the extension of the image  
-         * and returns the correct format for saving
-        */
+        ///<summary> 
+         /// Checks the extension of the image  
+         /// and returns the correct format for saving
+        ///</summary>
         public System.Drawing.Imaging.ImageFormat checkExtension(string file)
         {
             //Create variable for storing image type
@@ -67,14 +64,13 @@ namespace PAWA.Classes
             }
             return null;
         }
-        /*
-           *  Rename(string, string)
-           *  Take in original file name & new file name
-           * 
-           *  set tempName = newname + oldnames extension
-           *  eg. tempname = thenewname + .jpg
-           *      result   = thenewname.jpg
-        */
+        /// <summary>
+           ///  Take in original file name & new file name
+           /// 
+           ///  set tempName = newname + oldnames extension
+           ///  eg. tempname = thenewname + .jpg
+           ///      result   = thenewname.jpg
+        /// </summary>
         public string Rename(string original, string newName)
         {
             string tempName;
@@ -83,15 +79,14 @@ namespace PAWA.Classes
             return tempName;
         }
 
-        /*
-         * SeperateTags(string)
-         * Seperate the string via ','
-         * 
-         * Replace , with space
-         * cut up string via spaces and 
-         * store into array.
-         * return array.
-         */
+        ///<summary>
+         /// Seperate the string via ','
+         /// 
+         /// Replace , with space
+         /// cut up string via spaces and 
+         /// store into array.
+         /// return array.
+        ///</summary>
         public string[] seperateTags(string s)
         {
             string[] TagArr;
@@ -105,14 +100,12 @@ namespace PAWA.Classes
 
            return TagArr;
         }
-        /*
-         * checkIfTagExists(string[]) 
-         * 
-         * Will used the passed array to compare
-         * against the DB, if the tag doesnt exist
-         * it will create it. returning all of the
-         * images tags IDs in a List<int>
-        */
+        ///<summary>
+         /// Will used the passed array to compare
+         /// against the DB, if the tag doesnt exist
+         /// it will create it. returning all of the
+         /// images tags IDs in a List(int)
+        ///</summary>
         public List<int> checkIfTagExists(string[] TagArr)
         {
             PAWAContext db = new PAWAContext();
@@ -122,36 +115,63 @@ namespace PAWA.Classes
 
             foreach (string s in TagArr)
             {
+                //Convert tag to lowercase for safer use
+                string tagLower = s.ToLower();
+
                 //results == any rows in the db that match 's'
-                var results = db.Tags.Where(t => t.TagName.Contains(s));
+                var results = db.Tags.Where(t => t.TagName.Contains(tagLower));
+
                 //If tag does exist
                 if (results != null && results.Count() >= 1)
                 {
-                    System.Diagnostics.Debug.WriteLine(s +"Is in DB ");
-                    ImageTagsIDs.Add(getTagID(s));
+                    System.Diagnostics.Debug.WriteLine(tagLower +"Is in DB ");
+                    ImageTagsIDs.Add(getTagID(tagLower));
+                    TagUsedCountIncrease(tagLower);
                 }
                 //If tag doesnt exist
                 else
                 {
-                    System.Diagnostics.Debug.WriteLine("Is not in DB\nAdding new tag '" + s + "'...");
-                    createTag(s);
-                    System.Diagnostics.Debug.WriteLine("Tag  '" + s + "' added sucessfully.");
-                    ImageTagsIDs.Add(getTagID(s));
+                    System.Diagnostics.Debug.WriteLine("Is not in DB\nAdding new tag '" + tagLower + "'...");
+                    createTag(tagLower);
+                    System.Diagnostics.Debug.WriteLine("Tag  '" + tagLower + "' added sucessfully.");
+                    ImageTagsIDs.Add(getTagID(tagLower));
                 }
             } 
             return ImageTagsIDs;    
         }
+        ///<summary>
+         /// Increase the selected tags usedcount
+         /// in the database by 1 everytime this
+         /// method is called
+        ///</summary>
+        public void TagUsedCountIncrease(string tagName)
+        {
+            PAWAContext db = new PAWAContext();
+            
+            var tag =
+                from x in db.Tags
+                where x.TagName == tagName
+                select x;
 
+            foreach (var selectedTag in tag)
+            {
+                selectedTag.UseCount ++; 
+            }
+
+            db.SaveChanges();
+        }
+        ///<summary>
+         /// Combines all the tagIDs from a List
+         /// into a string for AlbumView Display
+        ///</summary>
         public string convertTagsToString(List<int> TagIDs)
         {
             string temp = string.Join(",", TagIDs);
             return temp;
         }
-        /*
-         * CreateTag(String) -- Users
-         * 
-         * Used to insert tags into the DB
-        */
+        ///<summary>
+         /// Used to insert tags into the DB
+        ///</summary>
         public void createTag(string name)
         {
             PAWAContext db = new PAWAContext();
@@ -166,12 +186,10 @@ namespace PAWA.Classes
             db.Tags.Add(newTag);
             db.SaveChanges();
         }
-       /*
-        * getTagID(string)
-        * 
-        * Used to get the tags id via
-        * passed tag name
-       */
+        ///<summary>
+        /// Used to get the tags id via
+        /// passed tag name
+        ///</summary>
         public int getTagID(string tag)
         {
             PAWAContext db = new PAWAContext();
@@ -188,11 +206,10 @@ namespace PAWA.Classes
 
             return 0;
         }
-        /*
-        * DeselectFile(int) 
-        * removes a file from the list of files
-        * set totalcount - 1
-       */
+        ///<summary>
+        /// removes a file from the list of files
+        /// set totalcount - 1
+        ///</summary>
         public void deselectFile(int id)
         {
             if (selectedfile.Contains(id))
@@ -202,42 +219,21 @@ namespace PAWA.Classes
             }
         }
 
-        /*
-         * SelectFile(int) 
-         * adds a file from the list of files
-         * set totalcount + 1
-        */
+        ///<summary>
+         /// SelectFile(int) 
+         /// adds a file from the list of files
+         /// set totalcount + 1
+        ///</summary>
         public List<int> selectFile(int id)
         {
             totalImageCount += 1;
             selectedfile[totalImageCount] = id;
             return selectedfile;
         }
-        
-        /*
-         * DeleteImage(List<int>) 
-         * Removes selected image(s) from the database
-        */
-        public void deleteImage(List<int> ids)
-        {
-            PAWAContext db = new PAWAContext();
-
-            foreach(int fileId in ids)
-            {
-                var deleteSelectedImages =
-                    from File in db.Files
-                    where File.FileID == fileId
-                    select File;
-                
-                db.Files.Remove(deleteSelectedImages.SingleOrDefault());  
-            }
-           
-
-            db.SaveChanges(); 
-           
-            //Reset count for next selection
-            totalImageCount = 0;
-        }
+        ///<summary>
+        /// Returns all of the users owned
+        /// folders in a List(folders)   
+        ///</summary>
         public IList<PAWA.Models.Folder> getFolders(int userID)
         {
             //open db connection
@@ -259,12 +255,10 @@ namespace PAWA.Classes
             }
             return list;
         }
-        /*
-         *  InsertImagetoDB(int,int,int,string,string,string,int)
-         * 
-         *  Uses all passed parimeters to create a new db 
-         *  image object, then pushes it into the db and saves  
-         */
+        ///<summary>
+         ///  Uses all passed parimeters to create a new db 
+         ///  image object, then pushes it into the db and saves  
+        ///</summary>
         public void insertImageToDB(int Height,int Width, int FileSize, string FileName, string Tags, string Description, int FolderID )
         {
             PAWAContext db = new PAWAContext();
@@ -289,13 +283,11 @@ namespace PAWA.Classes
             db.Files.Add(ImageFile);
             db.SaveChanges();
         }
-        /*
-         * PhotoValidation(HttpPostedFileBase) 
-         * 
-         * Confirms that the file uploaded is
-         * not null and is a image of the required
-         * type.
-         */
+        ///<summary>
+         /// Confirms that the file uploaded is
+         /// not null and is a image of the required
+         /// type.
+        ///</summary>
         public Boolean PhotoValidation(HttpPostedFileBase image)
         {
             if (image != null)
@@ -335,19 +327,7 @@ namespace PAWA.Classes
 
             return hexString.ToString() + "." + fileExtension;
         }
-       /* public int[] getTagIDs(string[] tags)
-        {
-            PAWAContext db = new PAWAContext();
-            int[] tagIDS;
-            
-            List<Models.Tags> TagDB;
-            var results = TagDB.Where(nc => nc.TagName.ToLower().StartsWith());
-
-           
-           
-
-            return
-        }*/
+     
 
     }
 }
