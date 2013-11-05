2f4ab0b91a79bb601558a8f662fcc100e4f63b9a
Jarrad Merrick <merrickj@live.com.au>
2013-11-05 14:18:53 +0800

    Added TestDbContext for testing purposes and PAWAContext interface.

---
+++ PAWA/DAL/IPAWAContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Data.Entity;
+using PAWA.Models;
+
+namespace PAWA.DAL
+{
+    public interface IPAWAContext
+    {
+        IDbSet<User> Users { get; set; }
+        IDbSet<Folder> Folders { get; set; }
+        IDbSet<PAWA.Models.Type> Types { get; set; }
+        IDbSet<Tags> Tags { get; set; }
+        IDbSet<File> Files { get; set; }
+        IDbSet<DailyStatistics> DailyStatistics { get; set; }
+
+        int SaveChanges();
+    }
+}
