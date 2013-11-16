a49da072b5e7be0e3d0d26049a849a7f6cf38dc7
Jarrad Merrick <merrickj@live.com.au>
2013-11-16 23:35:48 +0800

    Made ViewModel folder and moved AlbumViewModel.cs there.

---
+++ PAWA/ViewModels/AlbumViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PAWA.DAL;
+using PAWA.Classes;
+using PAWA.Models;
+
+namespace PAWA.ViewModels
+{
+    public class AlbumViewModel
+    {
+        public AlbumGrid ag;
+        public int? FolderID { get; set; }
+
+        public AlbumViewModel(IPAWAContext dbContext)
+        {
+            ag = new AlbumGrid(dbContext);
+        }
+    }
+}
