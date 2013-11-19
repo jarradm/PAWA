35d5dee2ffd44c2d779a0a990934882c51328c76
Kyle <kylec4114@gmail.com>
2013-11-19 10:59:58 +0800

    Create User Initial Commit

--- PAWA/Models/User.cs
+++ PAWA/Models/User.cs
@@ -20,20 +20,26 @@ namespace PAWA.Models
         [Required()]
         public string Username { get; set; }
 
-        [StringLength(50)]
-        [Required()]
+        [StringLength(50, ErrorMessage = "Must be up to 50 characters")]
+        [Required(ErrorMessage = "Password required.")]
         public string Password { get; set; }
 
-        [StringLength(100)]
-        [Required()]
+        [StringLength(100, MinimumLength = 8, ErrorMessage = "Must be between 8 and 100 characters")]
+        [Required(ErrorMessage = "Email required.")]
+        [EmailAddress(ErrorMessage = "Not a valid email address.")]
         public string Email { get; set; }
+
         public DateTime JoinDateTime { get; set; }
         public Status Status { get; set; }
         public DateTime? DeleteDateTime { get; set; }
 
-        [Required()]
+        [Required(ErrorMessage = "Country required.")]
         public string Country { get; set; }
+
+        [Required(ErrorMessage = "Date of Birth required.")]
         public DateTime DateOfBirth { get; set; }
+
+        [Required()]
         public Gender Gender { get; set; }
 
         public virtual ICollection<File> File { get; set; }
