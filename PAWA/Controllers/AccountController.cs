5ca16c15bab88befea917b86a52735654e912446
ShaneCurrie <shanec12@live.com>
2013-11-25 02:54:21 +0800

    Second Commit
    Includes .css file for form layout. Major changes to Account Controller,
    View Model added with custom Data Annotations. View updated to accept
    new View Model.

--- PAWA/Controllers/AccountController.cs
+++ PAWA/Controllers/AccountController.cs
@@ -3,6 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PAWA.DAL;
+using PAWA.Classes;
+using System.Data.Entity;
+using System.Reflection;
+using System.Data;
+using System.Globalization;
+using PAWA.ViewModels;
 
 namespace PAWA.Controllers
 {
@@ -31,16 +38,16 @@ namespace PAWA.Controllers
         {
             return View();
         }
+                
+        public ActionResult NewLogin()
+        {
+            return View();
+        }
 
         //
         // GET: /Account/AccountManagement
 
         public ActionResult AccountManagement()
-        {
-            return View();
-        }
-        
-        public ActionResult NewLogin()
         {
             return View();
         }
@@ -54,22 +61,53 @@ namespace PAWA.Controllers
             PAWAContext dbContext = new PAWAContext();
 
             var user = dbContext.Users.Where(u => u.UserID == 2).SingleOrDefault();
+            var uvm = new UserViewModel
+            {
+                UserID = user.UserID,
+                Email = user.Email,
+                Country = user.Country,
+                Gender = user.Gender,
+                Password = user.Password
+            };
 
-            return View(user);
+            ViewBag.CountryList = GetCountries();
+            return View(uvm);
         }
 
         //
         // POST: /Account/EditUser
 
+        public IEnumerable<SelectListItem> GetCountries()
+        {
+            RegionInfo country = new RegionInfo(new CultureInfo("en-US", false).LCID);
+            List<SelectListItem> countryNames = new List<SelectListItem>();
+
+            //To get the Country Names from the CultureInfo installed in windows
+            foreach (CultureInfo cul in CultureInfo.GetCultures(CultureTypes.SpecificCultures))
+            {
+                country = new RegionInfo(new CultureInfo(cul.Name, false).LCID);
+                countryNames.Add(new SelectListItem() { Text = country.DisplayName, Value = country.DisplayName });
+            }
+            
+            //Assigning all Country names to IEnumerable
+            IEnumerable<SelectListItem> nameAdded = countryNames.GroupBy(x => x.Text).Select(x => x.FirstOrDefault()).ToList<SelectListItem>().OrderBy(x => x.Text);
+            return nameAdded;
+        }
 
         [HttpPost]
-        public ActionResult EditUser(string button, FormCollection fc, string confirmPass, string pass)
+        public ActionResult EditUser(string button, UserViewModel uvm, FormCollection fc)
         {
             PAWAContext dbContext = new PAWAContext();
+            
             var user = dbContext.Users.Where(u => u.UserID == 2).SingleOrDefault();
-            string password = pass;
-            string confirmPassword = confirmPass;
-            var email = fc.Get(1).ToString();
+            string password = fc.Get(2).ToString();
+            string confirmPassword = fc.Get(3).ToString();
+            string email = fc.Get(1).ToString();
+            string country = fc.Get(4).ToString();
+            
+            //return Content("Password: " + password + "Confirmpassword: " + confirmPassword + "Email: " + email + "Country: " + country + "Gender: " + uvm.Gender);
+
+            ViewBag.CountryList = GetCountries();
 
             if (button == "cancel")
             {
@@ -81,43 +119,33 @@ namespace PAWA.Controllers
             }
             if (button == "submit")
             {
-                TryUpdateModel(user, fc);
+                TryUpdateModel(uvm);
                 if (ModelState.IsValid == true)
                 {
-                    if (password != "" && confirmPassword != "")
+                    if (!String.IsNullOrEmpty(email))
+                        user.Email = uvm.Email;
+                    if (String.IsNullOrEmpty(email))
+                        user.Email = user.Email;
+                    if (String.IsNullOrEmpty(password))
+                        user.Password = user.Password;
+                    if (password == confirmPassword)
                     {
-                        if (password == confirmPassword && password != "")
-                        {
-                            user.Password = password;
-                            dbContext.Entry(user).State = EntityState.Modified;
-                            dbContext.SaveChanges();
-                        }
-                        else
-                        {
-                            dbContext.GetValidationErrors();
-                        }
+                        uvm.Password = password;
+                        user.Password = uvm.Password;
                     }
-                    else
-                    {
-                        dbContext.GetValidationErrors();
-                    }
-                    if (email != "" || email != "" && password == confirmPassword)
-                    {
-                        user.Email = email;
-                        dbContext.Entry(user).State = EntityState.Modified;
-                        dbContext.SaveChanges();
-                    }
-                    else
-                    {
-                        dbContext.GetValidationErrors();
-                    }
+                    PAWA.Models.Gender g = (PAWA.Models.Gender)Enum.Parse(typeof(PAWA.Models.Gender), fc.Get(5));
+                    user.Gender = g;
+                    user.Country = uvm.Country;
                 }
                 else
                 {
                     dbContext.GetValidationErrors();
                 }
+                dbContext.Entry(user).State = EntityState.Modified;
+                dbContext.SaveChanges();
+
             }
-            return View(user);
+            return View(uvm);
 
         }
 
