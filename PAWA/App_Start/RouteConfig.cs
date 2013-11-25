f4c03e6a3d3427dfa4f261b032082d4d80ba9bbc
Jarrad Merrick <merrickj@live.com.au>
2013-11-25 16:11:49 +0800

    EditFolder: Changed RouteConfig.cs back to its default value.

--- PAWA/App_Start/RouteConfig.cs
+++ PAWA/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace PAWA
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Album", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
