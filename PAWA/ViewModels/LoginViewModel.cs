7f1141482c38b24a7d0a245cc965cef651065111
Thomas Fisher <thomasfihers@hotmail.com>
2013-12-04 14:44:14 +0800

    UpdateImageFinal fixes: UpdateImage.cshtml now has [Tag Cloud]

---
+++ PAWA/ViewModels/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.ComponentModel.DataAnnotations;
+
+namespace PAWA.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Username is required")]
+        public string Username { get; set; }
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+        public string IncorrectLogin { get; set; }
+    }
+}
