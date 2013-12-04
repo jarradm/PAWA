8e27f3d7cbb6c8ff1aeee5bf76e52e7b6af5ed34
Jarrad Merrick <merrickj@live.com.au>
2013-12-04 15:49:12 +0800

    UpdateImageFinal: Reverted LoginViewModel to latest master version.

--- PAWA/ViewModels/LoginViewModel.cs
+++ PAWA/ViewModels/LoginViewModel.cs
@@ -11,8 +11,7 @@ namespace PAWA.ViewModels
     {
         [Required(ErrorMessage = "Username is required")]
         public string Username { get; set; }
-        [Required(ErrorMessage = "Password is required")]
+        [Required(ErrorMessage="Password is required")]
         public string Password { get; set; }
-        public string IncorrectLogin { get; set; }
     }
 }
