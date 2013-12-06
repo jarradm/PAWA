d6e5f7095a3755cb765d08b0e0fe841a0e3fdea8
Jarrad Merrick <merrickj@live.com.au>
2013-12-06 12:31:56 +0800

    MovedFromModels: Added moved files.

---
+++ PAWA/DAL/IFolderRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PAWA.Models;
+
+namespace PAWA.DAL
+{
+    public interface IFolderRepository
+    {
+        IEnumerable<Folder> GetAll();
+        Folder Get(int id);
+        Folder Add(Folder item);
+        void Remove(int id);
+        bool Update(Folder item);
+    }
+}
